ac3a2c41c03e7491664708c21df2f650f603047d
Pierluigi97 <barsanofiopierluigimoretto@gmail.com>
2024-11-20 23:31:53 +0100

    feat: array 5. complete

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -26,8 +26,9 @@ console.log(lastTeacher);
 const firstTeacher = teachers.splice(0, 1);
 console.log(firstTeacher);
 
-
 // 5. Aggiungi un insegnante di nome 'Vanessa' alla fine dell'array teachers
+teachers.push('Vanessa');
+console.log(teachers);
 
 // 6. Aggiungi un insegnante di nome 'Sarah' all'inizio dell'array teachers
 
